3ddd8de5c233c8354fe848863f47eb038892682c
Tagir Valeev <lany@ngs.ru>
2015-04-11 14:56:06 +0600

    LICENSE (Apache 2.0)

--- src/main/java/javax/util/streamex/AbstractStreamEx.java
+++ src/main/java/javax/util/streamex/AbstractStreamEx.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2015 Tagir Valeev
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package javax.util.streamex;
 
 import java.util.Comparator;
@@ -21,6 +36,7 @@ import java.util.stream.DoubleStream;
 import java.util.stream.IntStream;
 import java.util.stream.LongStream;
 import java.util.stream.Stream;
+
 
 /* package */ abstract class AbstractStreamEx<T, S extends AbstractStreamEx<T, S>> implements Stream<T> {
 	final Stream<T> stream;
