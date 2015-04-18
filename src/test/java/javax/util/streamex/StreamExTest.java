ee3c38c683d3e1028fccc3360d9ea2debfa09aeb
Tagir Valeev <lany@ngs.ru>
2015-04-18 13:32:04 +0600

    StreamExTest: groupingBy tests

--- src/test/java/javax/util/streamex/StreamExTest.java
+++ src/test/java/javax/util/streamex/StreamExTest.java
@@ -21,14 +21,18 @@ import java.io.StringReader;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Function;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import org.junit.Test;
@@ -107,7 +111,41 @@ public class StreamExTest {
         assertEquals(expected3, parallelMap3);
         assertFalse(seqMap3 instanceof ConcurrentMap);
         assertTrue(parallelMap3 instanceof ConcurrentMap);
-        
+    }
+    
+    @Test
+    public void testGroupingBy() {
+        Map<Integer, List<String>> expected = new HashMap<>();
+        expected.put(1, Arrays.asList("a"));
+        expected.put(2, Arrays.asList("bb", "bb"));
+        expected.put(3, Arrays.asList("ccc"));
+        Map<Integer, List<String>> seqMap = StreamEx.of("a", "bb", "bb", "ccc").groupingBy(String::length);
+        Map<Integer, List<String>> parallelMap = StreamEx.of("a", "bb", "bb", "ccc").parallel().groupingBy(String::length);
+        assertEquals(expected, seqMap);
+        assertEquals(expected, parallelMap);
+        assertFalse(seqMap instanceof ConcurrentMap);
+        assertTrue(parallelMap instanceof ConcurrentMap);
+
+        Map<Integer, Set<String>> expectedMapSet = new HashMap<>();
+        expectedMapSet.put(1, new HashSet<>(Arrays.asList("a")));
+        expectedMapSet.put(2, new HashSet<>(Arrays.asList("bb", "bb")));
+        expectedMapSet.put(3, new HashSet<>(Arrays.asList("ccc")));
+        Map<Integer, Set<String>> seqMapSet = StreamEx.of("a", "bb", "bb", "ccc").groupingBy(String::length, Collectors.toSet());
+        Map<Integer, Set<String>> parallelMapSet = StreamEx.of("a", "bb", "bb", "ccc").parallel().groupingBy(String::length, Collectors.toSet());
+        assertEquals(expectedMapSet, seqMapSet);
+        assertEquals(expectedMapSet, parallelMapSet);
+        assertFalse(seqMapSet instanceof ConcurrentMap);
+        assertTrue(parallelMapSet instanceof ConcurrentMap);
+
+        seqMapSet = StreamEx.of("a", "bb", "bb", "ccc").groupingBy(String::length, HashMap::new, Collectors.toSet());
+        assertEquals(expectedMapSet, seqMapSet);
+        assertFalse(seqMapSet instanceof ConcurrentMap);
+        seqMapSet = StreamEx.of("a", "bb", "bb", "ccc").parallel().groupingBy(String::length, HashMap::new, Collectors.toSet());
+        assertEquals(expectedMapSet, seqMapSet);
+        assertFalse(seqMapSet instanceof ConcurrentMap);
+        parallelMapSet = StreamEx.of("a", "bb", "bb", "ccc").parallel().groupingBy(String::length, ConcurrentHashMap::new, Collectors.toSet());
+        assertEquals(expectedMapSet, parallelMapSet);
+        assertTrue(parallelMapSet instanceof ConcurrentMap);
     }
     
     @Test
