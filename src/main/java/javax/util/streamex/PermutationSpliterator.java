187baec953e42ba02bee6bb5d0529e3ee23453ea
Tagir Valeev <lany@ngs.ru>
2015-05-05 23:27:00 +0600

    PermutationSpliterator; tests

--- src/main/java/javax/util/streamex/PermutationSpliterator.java
+++ src/main/java/javax/util/streamex/PermutationSpliterator.java
@@ -1,81 +1,93 @@
 package javax.util.streamex;
 
+import java.util.BitSet;
 import java.util.Spliterator;
 import java.util.function.Consumer;
 
 final class PermutationSpliterator implements Spliterator<int[]> {
     private static final long[] factorials = new long[] { 1L, 1L, 2L, 6L, 24L, 120L, 720L, 5040L, 40320L, 362880L,
-        3628800L, 39916800L, 479001600L, 6227020800L, 87178291200L, 1307674368000L, 20922789888000L,
-        355687428096000L, 6402373705728000L, 121645100408832000L, 2432902008176640000L };
+            3628800L, 39916800L, 479001600L, 6227020800L, 87178291200L, 1307674368000L, 20922789888000L,
+            355687428096000L, 6402373705728000L, 121645100408832000L, 2432902008176640000L };
 
     private final int[] value;
-    private int nFixed;
-    private int hi;
     private long remainingSize;
-    
+    private long pos;
+
     public PermutationSpliterator(int length) {
-        this(init(length), 0, 0, length -1);
+        this(init(length), 0, getSize(length));
+    }
+
+    private static long getSize(int length) {
+        if (length > factorials.length)
+            throw new IllegalArgumentException("Length is too big");
+        return factorials[length];
     }
 
     private static int[] init(int length) {
         int[] initValue = new int[length];
-        for(int i=0; i<length; i++)
+        for (int i = 0; i < length; i++)
             initValue[i] = i;
         return initValue;
     }
 
-    public PermutationSpliterator(int[] startValue, int nFixed, int lo, int hi) {
+    public PermutationSpliterator(int[] startValue, long pos, long remainingSize) {
         this.value = startValue;
-        this.nFixed = nFixed;
-        this.hi = hi;
-        int len = startValue.length - nFixed;
-        if(len > factorials.length)
-            remainingSize = Long.MAX_VALUE;
-        else
-            remainingSize = factorials[len - 1] * (hi - lo + 1);
+        this.pos = pos;
+        this.remainingSize = remainingSize;
     }
-    
+
     @Override
     public boolean tryAdvance(Consumer<? super int[]> action) {
-        if(remainingSize == 0)
+        if (remainingSize == 0)
             return false;
         action.accept(value);
-        int k = value.length-2;
-        while(k >= nFixed && value[k] > value[k+1])
-            k--;
-        if (k < nFixed) {
-            remainingSize = 0;
-            return true;
-        }
-        int vk = value[k];
-        if (k == nFixed && vk == hi) {
-            remainingSize = 0;
-            return true;
-        }
-        int l = value.length-1;
-        while(vk > value[l])
-            l--;
-        value[k] = value[l];
-        value[l] = vk;
-        if (k == nFixed && value[k] == hi) {
-            nFixed++;
-            hi = value.length - 1;
-        }
-        int rem = value.length + k + 1;
-        for(int i=k+1; i<rem/2; i++)
-        {
-            int tmp = value[i];
-            value[i] = value[rem - 1 - i];
-            value[rem - 1 - i] = tmp;
-        }
         remainingSize--;
+        pos++;
+        if (remainingSize > 0) {
+            int k = value.length - 2;
+            while (value[k] > value[k + 1])
+                k--;
+            int vk = value[k];
+            int l = value.length - 1;
+            while (vk > value[l])
+                l--;
+            value[k] = value[l];
+            value[l] = vk;
+            int rem = value.length + k + 1;
+            for (int i = k + 1; i < rem / 2; i++) {
+                int tmp = value[i];
+                value[i] = value[rem - 1 - i];
+                value[rem - 1 - i] = tmp;
+            }
+        }
         return true;
     }
 
     @Override
     public Spliterator<int[]> trySplit() {
-        // TODO Auto-generated method stub
-        return null;
+        if (remainingSize <= 1)
+            return null;
+        int[] newValue = value.clone();
+        BitSet b = new BitSet();
+        long newRemainingSize = remainingSize / 2;
+        long newPos = pos + newRemainingSize;
+        long s = newPos;
+        for (int i = 0; i < value.length; i++) {
+            long f = factorials[value.length - i - 1];
+            int rem = (int)(s / f);
+            s %= f;
+            int idx = -1;
+            while (rem >= 0) {
+                idx = b.nextClearBit(idx+1);
+                rem--;
+            }
+            b.set(idx);
+            value[i] = idx;
+        }
+        PermutationSpliterator prefixSpliterator = new PermutationSpliterator(newValue, pos, newRemainingSize);
+        remainingSize = remainingSize - newRemainingSize;
+        pos = newPos;
+        return prefixSpliterator;
     }
 
     @Override
@@ -85,7 +97,7 @@ final class PermutationSpliterator implements Spliterator<int[]> {
 
     @Override
     public int characteristics() {
-        return ORDERED | NONNULL | IMMUTABLE | (remainingSize < Long.MAX_VALUE ? SIZED | SUBSIZED : null);
+        return ORDERED | NONNULL | IMMUTABLE | SIZED | SUBSIZED;
     }
 
 }
