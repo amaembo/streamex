c18d1713e794887749c2c52efcfc2691cf8cd843
Tagir Valeev <lany@ngs.ru>
2015-06-24 14:24:09 +0600

    package-info.java: streamops, reduction, mutable reduction

--- src/main/java/javax/util/streamex/package-info.java
+++ src/main/java/javax/util/streamex/package-info.java
@@ -17,26 +17,37 @@
  * This library provides enhancements for Java 8 Stream API. Public API contains the following classes and interfaces:
  * 
  * <p>
- * {@link javax.util.streamex.StreamEx}: implements {@link java.util.stream.Stream} and provides additional functionality for object streams.
+ * {@link StreamEx}: implements {@link java.util.stream.Stream} 
+ * and provides additional functionality for object streams.
  * 
  * <p>
- * {@link javax.util.streamex.IntStreamEx}, {@link javax.util.streamex.LongStreamEx}, {@link javax.util.streamex.DoubleStreamEx}: implement corresponding interfaces for primitive streams and enhancing them with additional functionality.
+ * {@link IntStreamEx}, {@link LongStreamEx}, {@link DoubleStreamEx}: 
+ * implement corresponding interfaces for primitive streams and enhancing them with additional functionality.
  *  
  * <p>
- * {@link javax.util.streamex.EntryStream}: implements {@link java.util.stream.Stream} of {@link java.util.Map.Entry} objects providing specific methods for operate on keys or values independently.
+ * {@link EntryStream}: implements {@link java.util.stream.Stream} of {@link java.util.Map.Entry} 
+ * objects providing specific methods for operate on keys or values independently.
  * 
  * <p>
- * Each of these classes contain a bunch of static methods to create the corresponding stream using different sources: collections, arrays, {@link java.io.Reader}, {@link java.util.Random} and so on.
+ * Each of these classes contain a bunch of static methods to create the corresponding stream using different sources: 
+ * collections, arrays, {@link java.io.Reader}, {@link java.util.Random} and so on.
  * 
  * <p>
- * {@link javax.util.streamex.IntCollector}, {@link javax.util.streamex.LongCollector}, {@link javax.util.streamex.DoubleCollector}: specialized collectors to work efficiently with primitive streams.
+ * {@link IntCollector}, {@link LongCollector}, {@link DoubleCollector}: 
+ * specialized collectors to work efficiently with primitive streams.
  * 
  * <p>
- * {@link javax.util.streamex.MoreCollectors}: utility class which provides a number of useful collectors which are absent in JDK {@link java.util.stream.Collectors} class.
+ * {@link MoreCollectors}: utility class which provides a number of useful collectors 
+ * which are absent in JDK {@link java.util.stream.Collectors} class.
+ * 
+ * <h2><a name="StreamOps">Stream operations and pipelines</a></h2>
+ * <p>StreamEx operations are divided into <em>intermediate</em>, <em>quasi-intermediate</em> and
+ * <em>terminal</em> operations, and are combined to form <em>stream
+ * pipelines</em>.  For more information about <em>intermediate</em> and <em>terminal</em> see the {@linkplain java.util.stream Stream API} documentation.
  * 
  * <p>
- * Most of new stream operations are either intermediate or terminal like it's defined in Java 8 Stream API. However due to the API limitations
- * a few operations are called "quasi-intermediate". In the most of the cases they behave as intermediate operations: for sequential stream there 
+ * In addition due to the API limitations new operation type is defined in StreamEx library which called "quasi-intermediate". 
+ * In the most of the cases they behave as intermediate operations: for sequential stream there 
  * should be no visible difference between intermediate and quasi-intermediate operation. The only known difference
  * is handling a parallel and unordered stream status. For intermediate operation there's no difference on calling {@code parallel()} 
  * before or after any intermediate operation. For quasi-intermediate operations if you call {@code parallel()} after the operation, then previous
@@ -47,8 +58,29 @@
  * Also the difference appears if you have an ordered stream source, but an unordered terminal operation (or collect using the unordered collector).
  * If you have only intermediate operations in-between, then all of them will be performed as unordered. However if you have a quasi-intermediate
  * operation, then unordered mode is not propagated through it, so the operations prior to the quasi-intermediate operation 
- * (including the quasi-intermediate operation itself) will remain ordered. 
+ * (including the quasi-intermediate operation itself) will remain ordered.
  * 
+ * <h2><a name="Reduction">Reduction operations</a></h2>
+ * 
+ * <p>
+ * A <em>reduction</em> operation takes a sequence of input elements and combines them into a single summary result. For more information see the {@linkplain java.util.stream Stream API} documentation.
+ * In addition to symmetrical reduction which requires reduction function to be associative, StreamEx library provides asymmetrical reduction methods
+ * like {@link StreamEx#foldLeft(Object, java.util.function.BiFunction)} and {@link StreamEx#foldRight(Object, java.util.function.BiFunction)}. These methods
+ * can be safely used for parallel streams, but the absence of associativity may lead to the performance drawback. Use them unly if you cannot provide
+ * an associative reduction function.   
+ * 
+ * <h3><a name="MutableReduction">Mutable reduction</a></h3>
+ *
+ * <p>
+ * A <em>mutable reduction operation</em> accumulates input elements into a
+ * mutable result container, such as a {@code Collection},
+ * as it processes the elements in the stream. The mutable reduction is usually performed via {@linkplain java.util.stream.Collector collectors}.
+ * See the {@linkplain java.util.stream Stream API} documentation for more details.
+ * 
+ * <p>
+ * StreamEx provides better support for mutable reduction on primitive streams. There are primitive collector classes {@link IntCollector}, {@link LongCollector} and {@link DoubleCollector}
+ * which extend {@link java.util.stream.Collector} interface, but capable to process primitive streams in more efficient way compared to using the boxed stream.
+ *
  * @author Tagir Valeev
  */
 package javax.util.streamex;
