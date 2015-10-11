3114e6a5b1e62e8b34f18382c2df3007006728fb
Tagir Valeev <lany@ngs.ru>
2015-10-11 12:40:55 +0600

    Revert "MoreCollectors#joining is removed (postponed to the next release; see limited-joiner branch)"
    This reverts commit 7bd8b86dc712f1281a51fe8abe054106eb524cdc.

--- src/test/java/javax/util/streamex/MoreCollectorsTest.java
+++ src/test/java/javax/util/streamex/MoreCollectorsTest.java
@@ -30,6 +30,7 @@ import java.util.EnumMap;
 import java.util.EnumSet;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
+import java.util.Locale;
 import java.util.List;
 import java.util.Map;
 import java.util.OptionalInt;
@@ -51,6 +52,7 @@ import java.util.stream.Collector.Characteristics;
 
 import javax.util.streamex.StreamExInternals.BooleanMap;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class MoreCollectorsTest {
@@ -305,14 +307,14 @@ public class MoreCollectorsTest {
         name2sex.put("Jane", "Girl");
         name2sex.put("Ruth", "Girl");
         name2sex.put("Melanie", "Girl");
-        Collector<Entry<String, String>, ?, Map<String, List<String>>> groupingBy = MoreCollectors.groupingBy(
+        Collector<Entry<String, String>, ?, Map<String, String>> groupingBy = MoreCollectors.groupingBy(
             Entry::getValue, StreamEx.of("Girl", "Boy").toSet(),
-            MoreCollectors.mapping(Entry::getKey, MoreCollectors.head(2)));
+            MoreCollectors.mapping(Entry::getKey, MoreCollectors.joining(", ", "...", 16, false)));
         AtomicInteger counter = new AtomicInteger();
-        Map<String, List<String>> map = EntryStream.of(name2sex).peek(c -> counter.incrementAndGet()).collect(groupingBy);
-        assertEquals(Arrays.asList("Mary", "Lucie"), map.get("Girl"));
-        assertEquals(Arrays.asList("John", "James"), map.get("Boy"));
-        assertEquals(4, counter.get());
+        Map<String, String> map = EntryStream.of(name2sex).peek(c -> counter.incrementAndGet()).collect(groupingBy);
+        assertEquals("Mary, Lucie, ...", map.get("Girl"));
+        assertEquals("John, James, ...", map.get("Boy"));
+        assertEquals(7, counter.get());
     }
 
     @Test
@@ -365,6 +367,41 @@ public class MoreCollectorsTest {
                 MoreCollectors.intersecting());
             checkCollectorEmpty("#" + i, Collections.emptySet(), MoreCollectors.intersecting());
         }
+    }
+
+    @Test
+    public void testJoining() {
+        List<String> input = Arrays.asList("one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+            "ten");
+        assertEquals("", StreamEx.of(input).peek(Assert::fail).collect(MoreCollectors.joining(", ", "...", 0, true)));
+        assertEquals("",
+            StreamEx.of(input).parallel().peek(Assert::fail).collect(MoreCollectors.joining(", ", "...", 0, true)));
+        String expected = "one, two, three, four, five, six, seven, eight, nine, ten";
+        for (int i = 3; i < expected.length() + 5; i++) {
+            String exp = expected;
+            if (exp.length() > i) {
+                exp = exp.substring(0, i - 3) + "...";
+            }
+            String exp2 = expected;
+            while (exp2.length() > i) {
+                int pos = exp2.lastIndexOf(", ", exp2.endsWith(", ...") ? exp2.length() - 6 : exp2.length());
+                exp2 = pos >= 0 ? exp2.substring(0, pos + 2) + "..." : "...";
+            }
+            for (StreamExSupplier<String> supplier : streamEx(input::stream)) {
+                assertEquals(supplier + "/#" + i, exp,
+                    supplier.get().collect(MoreCollectors.joining(", ", "...", i, true)));
+                assertEquals(supplier + "/#" + i, expected.substring(0, Math.min(i, expected.length())), supplier.get()
+                        .collect(MoreCollectors.joining(", ", "", i, true)));
+                assertEquals(supplier + "/#" + i, exp2,
+                    supplier.get().collect(MoreCollectors.joining(", ", "...", i, false)));
+            }
+        }
+
+        byte[] data = { (byte) 0xFF, 0x30, 0x40, 0x50, 0x70, 0x12, (byte) 0xF0 };
+        assertEquals(
+            "FF 30 40 50 ...",
+            IntStreamEx.of(data).mapToObj(b -> String.format(Locale.ENGLISH, "%02X", b & 0xFF))
+                    .collect(MoreCollectors.joining(" ", "...", 15, false)));
     }
 
     @Test
