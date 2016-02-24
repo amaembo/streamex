660d2187a9593b996ff401f71f2fa550f27d394c
Tagir Valeev <lany@ngs.ru>
2016-02-24 16:38:06 +0600

    [#70] LimiterTest: extract wars for javac 8<u40

--- src/test/java/one/util/streamex/LimiterTest.java
+++ src/test/java/one/util/streamex/LimiterTest.java
@@ -24,6 +24,7 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 /**
@@ -33,22 +34,26 @@ public class LimiterTest {
     @Test
     public void testLimiter() {
         Comparator<String> cmp = Comparator.nullsFirst(Comparator.comparingInt(String::length));
-        exerciseLimiter("str", Arrays.asList("abc", "abgdc", "abd", "a", "fgssdfg", "sfsvsx", null,
-            "wrffvs", "xcvbxvcb", "sffg", "abe", "adf", "abh"), cmp);
-        for(int i : new int[] {10, 100, 1000, 10000, 100000}) {
-            exerciseLimiter("asc, nat, "+i, IntStream.range(0, i).boxed().collect(Collectors.toList()), Comparator.naturalOrder());
-            exerciseLimiter("asc, dec, "+i, IntStream.range(0, i).boxed().collect(Collectors.toList()), Comparator.comparingInt(x -> x/10));
-            exerciseLimiter("desc, nat, "+i, IntStream.range(0, i).mapToObj(x -> ~x).collect(Collectors.toList()), Comparator.naturalOrder());
-            exerciseLimiter("desc, dec, "+i, IntStream.range(0, i).mapToObj(x -> ~x).collect(Collectors.toList()), Comparator.comparingInt(x -> x/10));
-            exerciseLimiter("rnd, nat, "+i, new Random(1).ints(i).boxed().collect(Collectors.toList()), Comparator.naturalOrder());
-            exerciseLimiter("rnd, dec, "+i, new Random(1).ints(i).boxed().collect(Collectors.toList()), Comparator.comparingInt(x -> x/10));
-            exerciseLimiter("rnd2, nat, "+i, new Random(1).ints(i, -1000, 1000).boxed().collect(Collectors.toList()), Comparator.naturalOrder());
-            exerciseLimiter("rnd2, dec, "+i, new Random(1).ints(i, -1000, 1000).boxed().collect(Collectors.toList()), Comparator.comparingInt(x -> x/10));
+        exerciseLimiter("str", Arrays.asList("abc", "abgdc", "abd", "a", "fgssdfg", "sfsvsx", null, "wrffvs",
+            "xcvbxvcb", "sffg", "abe", "adf", "abh"), cmp);
+        for (int i : new int[] { 10, 100, 1000, 10000, 100000 }) {
+            List<Integer> ascending = IntStream.range(0, i).boxed().collect(Collectors.toList());
+            exerciseLimiter("asc, nat, " + i, ascending, Comparator.naturalOrder());
+            exerciseLimiter("asc, dec, " + i, ascending, Comparator.comparingInt(x -> x / 10));
+            List<Integer> descending = IntStream.range(0, i).mapToObj(x -> ~x).collect(Collectors.toList());
+            exerciseLimiter("desc, nat, " + i, descending, Comparator.naturalOrder());
+            exerciseLimiter("desc, dec, " + i, descending, Comparator.comparingInt(x -> x / 10));
+            List<Integer> random = new Random(1).ints(i).boxed().collect(Collectors.toList());
+            exerciseLimiter("rnd, nat, " + i, random, Comparator.naturalOrder());
+            exerciseLimiter("rnd, dec, " + i, random, Comparator.comparingInt(x -> x / 10));
+            List<Integer> randomRange = new Random(1).ints(i, -1000, 1000).boxed().collect(Collectors.toList());
+            exerciseLimiter("rnd2, nat, " + i, randomRange, Comparator.naturalOrder());
+            exerciseLimiter("rnd2, dec, " + i, randomRange, Comparator.comparingInt(x -> x / 10));
         }
     }
-    
+
     public static <T> void exerciseLimiter(String msg, Collection<T> input, Comparator<T> comp) {
-        for(int limit : new int[] {0, 1, 2, 5, 10, 20, 100, 1000}) {
+        for (int limit : new int[] { 0, 1, 2, 5, 10, 20, 100, 1000 }) {
             exerciseLimiter(msg, input, limit, comp);
         }
     }
@@ -56,8 +61,8 @@ public class LimiterTest {
     public static <T> void exerciseLimiter(String msg, Collection<T> input, int limit, Comparator<T> comp) {
         List<T> expected = input.stream().sorted(comp).limit(limit).collect(Collectors.toList());
         List<T> actual = input.stream().collect(MoreCollectors.least(comp, limit));
-        assertEquals("Mismatch (sequential), "+msg+", limit="+limit, expected, actual);
+        assertEquals("Mismatch (sequential), " + msg + ", limit=" + limit, expected, actual);
         actual = input.parallelStream().collect(MoreCollectors.least(comp, limit));
-        assertEquals("Mismatch (parallel), "+msg+", limit="+limit, expected, actual);
+        assertEquals("Mismatch (parallel), " + msg + ", limit=" + limit, expected, actual);
     }
 }
