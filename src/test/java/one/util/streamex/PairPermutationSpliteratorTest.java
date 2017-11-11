9ee0485e2c1eb2f18d239a16fc6cc9b52c2ab350
Tagir Valeev <amaembo@gmail.com>
2017-11-11 14:38:06 +0700

    Copyrights updated project-wide

--- src/test/java/one/util/streamex/PairPermutationSpliteratorTest.java
+++ src/test/java/one/util/streamex/PairPermutationSpliteratorTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015, 2016 Tagir Valeev
+ * Copyright 2015, 2017 StreamEx contributors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
